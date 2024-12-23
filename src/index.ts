e88f7b7493099d953160caf9e74ea5effe6185bd
Antartic4 <robjose1996@hotmail.com>
2024-12-22 23:50:25 -0400

    not deploying on railway

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import taskRoutes from './routes/tasks';
 
-const app = express();
-const prisma = new PrismaClient();
+dotenv.config();
 
-app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
+const app = express();
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://rjs-todolist-frontend-production.up.railway.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
